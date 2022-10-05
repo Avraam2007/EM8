0a40ab1f5cc42847164634a5abdebdcd1fabf5da
AndriiAvramenk <72814303+AndriiAvramenk@users.noreply.github.com>
2022-10-05 19:51:01 +0300

    MyFile.js

--- MyFile.js
+++ MyFile.js
@@ -1,1 +1,14 @@
 alert("Hello world!")
+
+let a = 0;
+
+while(a>=0) {
+    a++;
+}
+
+let b = a--;
+let info = prompt("Who are you?");
+console.log(b);
+if (a===0) {
+    console.log(`Hello, ${info}`);
+}
